ae768a339f89c8dd4cb3f67e2edda5c25480dae2
GZMaster <33158650+GZMaster@users.noreply.github.com>
2023-04-28 23:09:22 +0100

    quest 10

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,28 +2,29 @@ import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-waffle";
 
-const RECOVERY_PHRASE = "INSERT_YOUR_PRIVATE_KEY_HERE";
+const RECOVERY_PHRASE =
+	"message day amazing gift morning spatial pen deer beef wood lunch kiwi brown often provide buffalo miracle coast rude cause badge fragile invite tomato";
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+	const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
+	for (const account of accounts) {
+		console.log(account.address);
+	}
 });
 
 const config: HardhatUserConfig = {
-  solidity: "0.7.0",
+	solidity: "0.7.0",
 
-  networks: {
-    fuji: {
-      url: "https://api.avax-test.network/ext/bc/C/rpc",
+	networks: {
+		fuji: {
+			url: "https://api.avax-test.network/ext/bc/C/rpc",
 
-      accounts: {
-        mnemonic: RECOVERY_PHRASE,
-      },
-    },
-  },
+			accounts: {
+				mnemonic: RECOVERY_PHRASE,
+			},
+		},
+	},
 };
 
 export default config;
