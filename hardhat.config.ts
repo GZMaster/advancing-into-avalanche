48e5a2e094bb5ddc702c373dff2621497b2b930c
GZMaster <33158650+GZMaster@users.noreply.github.com>
2023-04-28 23:11:02 +0100

    quest 8

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,7 +3,7 @@ import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-chai-matchers";
 import "@nomiclabs/hardhat-ethers";
 
-const RECOVERY_PHRASE = "INSERT_YOUR_PRIVATE_KEY_HERE";
+const RECOVERY_PHRASE = "message day amazing gift morning spatial pen deer beef wood lunch kiwi brown often provide buffalo miracle coast rude cause badge fragile invite tomato";
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
